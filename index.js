8e8f60a27b6eb6d75775609fc12b366cdc5280e6
Michelle Wang <oc3anazns3a@gmail.com>
2020-12-22 02:33:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 function destructivelyAppendKitten (name) {
   kittens.push(name);
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  return [... kittens, name]
+}
